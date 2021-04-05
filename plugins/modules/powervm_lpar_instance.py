3f636229c355362819ff3acf490431c020d358ab
Hari G M <harimura@in.ibm.com>
2021-04-05 18:55:01 +0530

    Updated Delete Partition flows to include clean up of VIOS config and Virtual Disks

--- plugins/modules/powervm_lpar_instance.py
+++ plugins/modules/powervm_lpar_instance.py
@@ -343,57 +343,20 @@ def create_partition(module, params):
 
 
 def remove_partition(module, params):
-    changed = False
-    rest_conn = None
-    system_uuid = None
     validate_parameters(params)
     hmc_host = params['hmc_host']
     hmc_user = params['hmc_auth']['username']
     password = params['hmc_auth']['password']
     system_name = params['system_name']
     vm_name = params['vm_name']
+    retainViosCfg = params['retain_vios_cfg']
+    deleteVdisks = params['delete_vdisks']
+    
+    hmc_conn = HmcCliConnection(module, hmc_host, hmc_user, password)
+    hmc = Hmc(hmc_conn)
 
-    try:
-        rest_conn = HmcRestClient(hmc_host, hmc_user, password)
-    except Exception as error:
-        logger.debug(repr(error))
-        module.fail_json(msg="Logon to HMC failed")
-
-    try:
-        system_uuid, server_dom = rest_conn.getManagedSystem(system_name)
-    except Exception as error:
-        try:
-            rest_conn.logoff()
-        except Exception:
-            logger.debug("Logoff error")
-        error_msg = parse_error_response(error)
-        module.fail_json(msg=error_msg)
-    if not system_uuid:
-        module.fail_json(msg="Given system is not present")
-
-    try:
-        partition_uuid, partition_dom = rest_conn.getLogicalPartition(system_uuid, vm_name)
-        if not partition_dom:
-            logger.debug("Given partition already absent on the managed system")
-            return False, None
-
-        if partition_dom.xpath("//PartitionState")[0].text != 'not activated':
-            module.fail_json(msg="Given logical partition:{0} is not in shutdown state".format(vm_name))
-
-        rest_conn.deleteLogicalPartition(partition_uuid)
-        changed = True
-    except Exception as error:
-        error_msg = parse_error_response(error)
-        logger.debug("Line number: %d exception: %s", sys.exc_info()[2].tb_lineno, repr(error))
-        module.fail_json(msg=error_msg)
-    finally:
-        try:
-            rest_conn.logoff()
-        except Exception as logoff_error:
-            error_msg = parse_error_response(logoff_error)
-            module.warn(error_msg)
-
-    return changed, None
+    hmc.deletePartition(system_name, vm_name, not(retainViosCfg), deleteVdisks)    
+    return True, None
 
 
 def perform_task(module):
@@ -419,7 +382,7 @@ def run_module():
                       no_log=True,
                       options=dict(
                           username=dict(required=True, type='str'),
-                          password=dict(required=True, type='str'),
+                          password=dict(type='str'),
                       )
                       ),
         system_name=dict(type='str', required=True),
@@ -427,6 +390,8 @@ def run_module():
         proc=dict(type='int'),
         mem=dict(type='int'),
         os_type=dict(type='str', choices=['aix', 'linux', 'aix_linux', 'ibmi']),
+        retain_vios_cfg=dict(type='bool'),
+        delete_vdisks=dict(type='bool'),
         state=dict(required=True, type='str',
                    choices=['present', 'absent'])
     )
